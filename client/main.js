3d35325c8dbaa65960a52ba88df7f926b3c2b061
Tommyluo <tluo12321@yahoo.com>
2015-03-23 15:41:46 -0700

    added flip animation to game pieces and adjusted some css

--- client/main.js
+++ client/main.js
@@ -1,3 +1,4 @@
+/* global _ */
 'use strict';
 
 $(document).ready();
@@ -19,13 +20,11 @@ $(function() {
     $('#gameboard').append(row);
   }
 
-
   //create preset game pieces load display
-  var set1combo1 = [$('#0'), $('#1'), $('#2'), $('#3'), $('#4'), $('#5'), $('#6'),
-                $('#7'), $('#8'), $('#9'), $('#10'), $('#11'), $('#12'), $('#13'),
-                $('#14'), $('#15'), $('#16'), $('#17'), $('#19'), $('#20'), $('#22'),
-                $('#23')];
-  // console.log(set1combo1.length);
+  // var set1combo1 = [$('#0'), $('#1'), $('#2'), $('#3'), $('#4'), $('#5'), $('#6'),
+  //               $('#7'), $('#8'), $('#9'), $('#10'), $('#11'), $('#12'), $('#13'),
+  //               $('#14'), $('#15'), $('#16'), $('#17'), $('#19'), $('#20'), $('#22'),
+  //               $('#23')];
 
   var set2combo1 = [$('#48'), $('#49'), $('#51'), $('#52'), $('#54'), $('#55'), $('#56'),
                 $('#57'), $('#58'), $('#59'), $('#60'), $('#61'), $('#62'), $('#63'),
@@ -77,6 +76,7 @@ $(function() {
   $('#35').addClass('snow');
   $('#36').addClass('snow');
   $('#39').addClass('snow');
+  $('.snow').html('Tundra');
 });
 
 //Defining game piece stats
@@ -136,8 +136,8 @@ $.getJSON(url, function(data) {
     var $charSet1Div = $('<div id="' + e.name + '" class="' + e.classType + '">');
     var imgData = data.data[e.name].image.full;
     var getImg = 'http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/' + imgData;
-    $charSet1Div.html('<img id="gamePiece' + count++ + '" class="gamePieces" src="' + getImg + '" title="' + [e.name] + '"/>');
-    // $('#set1').append($charSet1Div);
+    $charSet1Div.html('<img id="gamePiece' + count++ + '" class="gamePiecesImg" src="' + getImg + '" title="' + [e.name] + '"/>');
+
     set1Array.push($charSet1Div);
 
     if (e.classType === 'Chief1') {
@@ -170,7 +170,7 @@ $.getJSON(url, function(data) {
     var $charSet2Div = $('<div id="' + e.name + '" class="' + e.classType + '">');
     var imgData = data.data[e.name].image.full;
     var getImg = 'http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/' + imgData;
-    $charSet2Div.html('<img id="gamePiece' + count++ + '" class="gamePieces" src="' + getImg + '" title="' + [e.name] + '"/>');
+    $charSet2Div.html('<img id="gamePiece' + count++ + '" class="gamePiecesImg" src="' + getImg + '" title="' + [e.name] + '"/>');
     // $('#set2').append($charSet2Div);
     set2Array.push($charSet2Div);
     // set2BoardDisplay.push($charSet2Div);
@@ -212,8 +212,8 @@ $.getJSON(url, function(data) {
     var imgData2 = data.data[num2].image.full;
     var getImg1 = 'http://ddragon.leagueoflegends.com/cdn/5.2.1/img/item/' + imgData1;
     var getImg2 = 'http://ddragon.leagueoflegends.com/cdn/5.2.1/img/item/' + imgData2;
-    $flagDiv1.html('<img id="flagPiece1' + count++ + '" class="gamePieces" src="' + getImg1 + '" title="Banner of Command"/>');
-    $flagDiv2.html('<img id="flagPiece2' + count++ + '" class="gamePieces" src="' + getImg2 + '" title="Seraph\'s Embrace"/>');
+    $flagDiv1.html('<img id="flagPiece1' + count++ + '" class="gamePiecesImg" src="' + getImg1 + '" title="Banner of Command"/>');
+    $flagDiv2.html('<img id="flagPiece2' + count++ + '" class="gamePiecesImg" src="' + getImg2 + '" title="Seraph\'s Embrace"/>');
 
     if (flagset[0].classType === 'Flag1') {
       $('#chiefSet1').append('Flag', $flagDiv1);
@@ -236,7 +236,8 @@ $.getJSON(url, function(data) {
   var set1BoardDisplay = [];
   var set2BoardDisplay = [];
 
-  var charsetCopy1 = [
+  var player1Pieces =
+  [
     {name: 'Tryndamere', id: 'Tryndamere1', classType: 'ChiefCopy1'},
     {name: 'Teemo', id: 'Teemo1', classType: 'RunnerCopy1'},
     {name: 'Jax', id: 'Jax1', classType: 'FighterCopy1'},
@@ -252,15 +253,37 @@ $.getJSON(url, function(data) {
     {name: 'Zed', id: 'Zed1', classType: 'AssassinCopy1'}
   ];
 
-  charsetCopy1.forEach(function(e) {
-    var $charSet1CopyDiv = $('<div id="' + e.id + '" class="' + e.classType + ' draggable ui-widget-content redTeam">');
+  player1Pieces.forEach(function(e) {
+    var p1GamePieceDiv = $('<div class="' + e.classType + ' draggable ui-widget-content redTeam">');
+
+    var $flipper = $('<div>');
+    $flipper.addClass('flipper');
+
+    var $frontCover = $('<div>');
+    $frontCover.addClass('front');
+    $frontCover.html('<img class="coverImg" src="http://fc08.deviantart.net/fs70/f/2012/164/3/3/league_of_legends_dock_icon_by_kaldrax-d53bbj5.png">');
+
     var imgData = data.data[e.name].image.full;
     var getImg = 'http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/' + imgData;
-    $charSet1CopyDiv.html('<img id="gamePieceCopy' + count++ + '" class="gamePieces" src="' + getImg + '" title="' + [e.name] + '"/>');
-    set1BoardDisplay.push($charSet1CopyDiv);
+    // p1GamePieceDiv.html('<img id="gamePieceCopy' + count++ + '" class="gamePieces" src="' + getImg + '" title="' + [e.name] + '"/>');
+
+    var $back = $('<div id="' + e.id + '">');
+    $back.addClass('back');
+    $back.html('<img id="gamePieceCopy' + count++ + '" class="gamePiecesImg" src="' + getImg + '" title="' + [e.name] + '"/>');
+
+    $flipper.append($frontCover, $back);
+    p1GamePieceDiv.append($flipper);
+
+    set1BoardDisplay.push(p1GamePieceDiv);
     // console.log('set1', set1BoardDisplay);
 
-    $charSet1CopyDiv.draggable({
+    // create flip functionality
+    p1GamePieceDiv.click(function() {
+      $(this).find('.flipper').addClass('flip');
+      console.log('clicked');
+    });
+
+    p1GamePieceDiv.draggable({
       grid: [10, 10],
       containment: '#gameboard',
       stack: '.redTeam',
@@ -277,22 +300,53 @@ $.getJSON(url, function(data) {
   $.getJSON(itemUrl, function(data) {
     var num1 = 3060;
     var num2 = 3040;
-    var $flagCopyDiv1 = $('<div id="Banner of Command1" class="FlagCopy1 draggable ui-widget-content">');
+    var $flagCopyDiv1 = $('<div class="FlagCopy1 draggable ui-widget-content">');
     var $flagCopyDiv2 = $('<div id="Seraph\'s Embrace2" class="FlagCopy2 draggable ui-widget-content">');
+
+    var $flipper1 = $('<div>');
+    $flipper1.addClass('flipper');
+    var $flipper2 = $('<div>');
+    $flipper2.addClass('flipper');
+
+    var $frontCover1 = $('<div>');
+    $frontCover1.addClass('front');
+    $frontCover1.html('<img class="coverImg" src="http://fc08.deviantart.net/fs70/f/2012/164/3/3/league_of_legends_dock_icon_by_kaldrax-d53bbj5.png">');
+    var $frontCover2 = $('<div>');
+    $frontCover2.addClass('front');
+    $frontCover2.html('<img class="coverImg" src="http://fc08.deviantart.net/fs70/f/2012/164/3/3/league_of_legends_dock_icon_by_kaldrax-d53bbj5.png">');
+
     var imgData1 = data.data[num1].image.full;
     var imgData2 = data.data[num2].image.full;
     var getImg1 = 'http://ddragon.leagueoflegends.com/cdn/5.2.1/img/item/' + imgData1;
     var getImg2 = 'http://ddragon.leagueoflegends.com/cdn/5.2.1/img/item/' + imgData2;
-    $flagCopyDiv1.html('<img id="flagPieceCopy1' + count++ + '" class="gamePieces" src="' + getImg1 + '" title="Banner of Command"/>');
-    $flagCopyDiv2.html('<img id="flagPieceCopy2' + count++ + '" class="gamePieces" src="' + getImg2 + '" title="Seraph\'s Embrace"/>');
+    // $flagCopyDiv1.html('<img id="flagPieceCopy1' + count++ + '" class="gamePiecesImg" src="' + getImg1 + '" title="Banner of Command"/>');
+    // $flagCopyDiv2.html('<img id="flagPieceCopy2' + count++ + '" class="gamePiecesImg" src="' + getImg2 + '" title="Seraph\'s Embrace"/>');
+
+    var $back1 = $('<div id="Banner of Command1">');
+    $back1.addClass('back');
+    $back1.html('<img id="gamePieceCopy' + count++ + '" class="gamePiecesImg" src="' + getImg1 + '" title="Banner of Command"/>');
+    $flipper1.append($frontCover1, $back1);
+    $flagCopyDiv1.append($flipper1);
+
+    var $back2 = $('<div id="Banner of Command1">');
+    $back2.addClass('back');
+    $back2.html('<img id="gamePieceCopy' + count++ + '" class="gamePiecesImg" src="' + getImg2 + '" title="Seraph\'s Embrace"/>');
+    $flipper2.append($frontCover2, $back2);
+    $flagCopyDiv2.append($flipper2);
+
     set1BoardDisplay.push($flagCopyDiv1);
     set2BoardDisplay.push($flagCopyDiv2);
     // console.log(set2BoardDisplay);
 
-    //randomize red side game pieces
+    $flagCopyDiv1.click(function() {
+      $(this).find('.flipper').addClass('flip');
+    });
+
+    $flagCopyDiv2.click(function() {
+      $(this).find('.flipper').addClass('flip');
+    });
+    //randomize game pieces
     set1BoardDisplay = _.shuffle(set1BoardDisplay);
-    console.log(set1BoardDisplay);
-    //randomize blue side game pieces
     set2BoardDisplay = _.shuffle(set2BoardDisplay);
 
     // $flagCopyDiv1.draggable({
@@ -310,7 +364,13 @@ $.getJSON(url, function(data) {
 
     //place game pieces onto gameboard
     for (var i = 0; i < set1BoardDisplay.length; i++) {
-      $('#' + i).append(set1BoardDisplay[i]);
+      // $('#' + i).append(set1BoardDisplay[i]);
+      //create preset game pieces load display
+      var set1combo1 = [$('#0'), $('#1'), $('#2'), $('#3'), $('#4'), $('#5'), $('#6'),
+                    $('#7'), $('#8'), $('#9'), $('#10'), $('#11'), $('#12'), $('#13'),
+                    $('#14'), $('#15'), $('#16'), $('#17'), $('#19'), $('#20'), $('#22'),
+                    $('#23')];
+      set1combo1.append(set1BoardDisplay[i]);
     }
 
     for (var i = 0, j = 71; i < set2BoardDisplay.length; j--, i++) {
@@ -319,7 +379,7 @@ $.getJSON(url, function(data) {
     }
   });
 
-  var charsetCopy2 = [
+  var player2Pieces = [
     {name: 'JarvanIV', id: 'JarvanIV2', classType: 'ChiefCopy2'},
     {name: 'Singed', id: 'Singed2', classType: 'RunnerCopy2'},
     {name: 'Garen', id: 'Garen2', classType: 'FighterCopy2'},
@@ -335,15 +395,37 @@ $.getJSON(url, function(data) {
     {name: 'Katarina', id: 'Katarina2', classType: 'AssassinCopy2'}
   ];
 
-  charsetCopy2.forEach(function(e) {
-    var $charSet2CopyDiv = $('<div id="' + e.id + '" class="' + e.classType + ' draggable ui-widget-content blueTeam">');
+  player2Pieces.forEach(function(e) {
+    var p2GamePieceDiv = $('<div class="' + e.classType + ' draggable ui-widget-content blueTeam">');
+
+    var $flipper = $('<div>');
+    $flipper.addClass('flipper');
+
+    var $frontCover = $('<div>');
+    $frontCover.addClass('front');
+    $frontCover.html('<img class="coverImg" src="http://fc08.deviantart.net/fs70/f/2012/164/3/3/league_of_legends_dock_icon_by_kaldrax-d53bbj5.png">');
+
     var imgData = data.data[e.name].image.full;
     var getImg = 'http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/' + imgData;
-    $charSet2CopyDiv.html('<img id="gamePieceCopy' + count++ + '" class="gamePieces" src="' + getImg + '" title="' + [e.name] + '"/>');
-    set2BoardDisplay.push($charSet2CopyDiv);
+    // p2GamePieceDiv.html('<img id="gamePieceCopy' + count++ + '" class="gamePiecesImg" src="' + getImg + '" title="' + [e.name] + '"/>');
+
+    var $back = $('<div id="' + e.id + '">');
+    $back.addClass('back');
+    $back.html('<img id="gamePieceCopy' + count++ + '" class="gamePiecesImg" src="' + getImg + '" title="' + [e.name] + '"/>');
+
+    $flipper.append($frontCover, $back);
+    p2GamePieceDiv.append($flipper);
+
+    set2BoardDisplay.push(p2GamePieceDiv);
     // console.log(set2BoardDisplay);
 
-    $charSet2CopyDiv.draggable({
+    // create flip functionality
+    p2GamePieceDiv.click(function() {
+      $(this).find('.flipper').addClass('flip');
+      console.log('clicked');
+    });
+
+    p2GamePieceDiv.draggable({
       grid: [10, 10],
       containment: '#gameboard',
       stack: 'blueTeam',
@@ -351,37 +433,20 @@ $.getJSON(url, function(data) {
       revert: true});
   });
 
-    //create animation for drag and drop action
-    $('#gameboard').find('div').droppable({
-      accept: '.draggable',
-      hoverClass: 'hovered',
-      drop: acceptGamePieces
-    });
+  //create animation for drag and drop action
+  $('#gameboard').find('div').droppable({
+    accept: '.draggable',
+    hoverClass: 'hovered',
+    drop: acceptGamePieces
+  });
 
-    function acceptGamePieces(event, ui) {
-      var gamePiece = $(this);
-      if (gamePiece) {
-        ui.draggable.addClass('acceptPiece');
-        // ui.draggable.draggable('disable');
-        ui.draggable.position( { of: $(this), my: 'center', at: 'center' } );
-        ui.draggable.draggable( 'option', 'revert', false );
-      }
+  function acceptGamePieces(event, ui) {
+    var gamePiece = $(this);
+    if (gamePiece) {
+      ui.draggable.addClass('acceptPiece');
+      // ui.draggable.draggable('disable');
+      ui.draggable.position({of: $(this), my: 'center', at: 'center'});
+      ui.draggable.draggable('option', 'revert', false);
     }
-
-  //   $('#gameboard').find('div').droppable({
-  //     accept: '.blueTeam',
-  //     hoverClass: 'hovered',
-  //     drop: acceptBlueGamePieces
-  //   });
-  //
-  // function acceptBlueGamePieces(event, ui) {
-  //   var gamePiece = $(this);
-  //   if (gamePiece) {
-  //     ui.draggable.addClass('acceptPiece');
-  //     // ui.draggable.draggable('disable');
-  //     ui.draggable.position( { of: $(this), my: 'center', at: 'center' } );
-  //     ui.draggable.draggable( 'option', 'revert', false );
-  //   }
-  // }
-
+  }
 });
